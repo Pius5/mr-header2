8d65c4e83a778b7ab50d5cc59ddacdf6f6d57b99
Pius Motai <Pius Motai@DESKTOP-6PNFMH0>
2022-02-26 19:36:01 +0200

    Working with headers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import cat from './cat.png';
+import cat4 from './cat4.jpg';
 import './App.css';
+import Info from './Info';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+
+        <><div>
+
+            <img src={cat} height="50px" width="50px" alt="Yes" />
+            <div className="App">
+
+
+                <ul>
+
+                    <li>Menu</li>
+                    <li>Services</li>
+                    <li>Contact Us</li>
+                    <li>About</li>
+
+
+                </ul>
+                <hr />
+                <Info />
+
+            </div>
+
+            <img src={cat4} height="300px" width="400px" alt="Yes" id="cat4" />
+            <br />
+            <br />
+
+
+        </div><p id="copy"><a href="https://www.bestcat.com/">&copy; Cat Health Care Services, 2022</a></p></>
   );
 }
 
---
+++ src/Info.js
@@ -0,0 +1,16 @@
+function Info() {
+    return (
+
+        <div>
+
+            <h2 id="cathead">MY CAT HEALTH CARE</h2>
+            <br/>
+            <p id="catinfo">
+                Cats are sometimes notorious for trying to hide their illness. Be on the look out for that. Prompt treatment is necessary for any disease especially for heartworms. Some plants are dangerous or even poisous to cats. You are advised to surf the internet to identify which of the common house plants may be deadly to your feline friend. As an advice cat owners are advised not to declaw their cats because without claws, a cat cannot defend itself. Adult cats should not have bad breath as this can be due to a tooth decay or other oral disease. So take care of your cats.
+
+            </p>
+        </div>
+    );
+}
+
+export default Info;
